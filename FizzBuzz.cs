4d4d8e6b55e2207b7ecca3d1dbe5c8f66ad313be
Grant <grant.wright@bglgroup.co.uk>
2018-07-13 11:20:06 +0100

    created our fizz buzz bang app

---
+++ FizzBuzz.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string GenerateFizzBuzzResults(int input)
+        {
+            if (input < 0)
+            {
+                throw new ArgumentException("Input value must be positive");
+            }
+
+            if (input == 0)
+            {
+                throw new ArgumentException("Input value must be greater than 0");
+            }
+
+            string result = "";
+            
+            for (int i = 1; i <= input; i++)
+            {
+                var isSquaredNumber = Math.Sqrt(double.Parse(i.ToString())) % 1 == 0;
+
+                if (i % 3 == 0 && i % 5 == 0 && isSquaredNumber)
+                {
+                    result += "FizzBuzzBang,";
+                }
+                else if (i % 3 == 0 && i % 5 == 0)
+                {
+                    result += "FizzBuzz,";
+                }
+                else
+                {
+                    if (i % 3 == 0 && isSquaredNumber)
+                    {
+                        result += "FizzBang,";
+                    }
+                    else if (i % 5 == 0 && isSquaredNumber)
+                    {
+                        result += "BuzzBang,";
+                    }
+                    else if (i % 3 == 0)
+                    {
+                        result += "Fizz,";
+                    }
+                    else if (i % 5 == 0)
+                    {
+                        result += "Buzz,";
+                    }
+                    else if (isSquaredNumber)
+                    {
+                        result += "Bang,";
+                    }
+                    else
+                    {
+                        result += i + ",";
+                    }
+                }
+            }
+
+            return result.TrimEnd(',');
+        }
+    }
+}
